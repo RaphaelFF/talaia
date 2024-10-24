dd8550c7ba78fa326f2551cdbf64e9703aa9a03b
Raphael Fernandes Franca <116182607+RaphaelFF@users.noreply.github.com>
2024-10-24 10:58:31 -0300

    usando componentes

--- App.js
+++ App.js
@@ -4,17 +4,16 @@ import Home from './src/home/home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Testando texto no react native</Text>
-      <Home />
-      <StatusBar style="auto" />
-    </View>
+    
+      <Home/>
+    
+   
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
--- src/home/home.js
+++ src/home/home.js
@@ -1,18 +1,43 @@
-import {Text, View, StyleSheet} from "react-native";
+import { Text, View, StyleSheet, Image, ImageBackground, TouchableOpacity } from "react-native";
 
-export default function Home(){
+const Home = () => {
     return (
-        <View>
-            <Text>testando a pagina home</Text>
-        </View>
+        <ImageBackground style={estilo.fundo} source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaQ7PSbLfsn-DuI2YTo7K1L1s2o5MpxURkCw&s' }} >
+            <View style={estilo.tela}>
+
+                <TouchableOpacity style={estilo.tela}>
+                    <Image style={estilo.image} source={{
+                        uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc1ryw139eeE7PfK_CY_IT5MJwfNJfaWXkKg&s'
+                    }}
+
+                    />
+
+                    <Text style={{ color: '#fff', marginTop: 10, fontSize: 24 }}>Tradutor de linguagem Juridicas</Text>
+                </TouchableOpacity>
+            </View>
+        </ImageBackground>
     );
 }
 
 const estilo = StyleSheet.create({
-    tela:{
+    tela: {
         flex: 1,
         justifyContent: "center",
-        backgroundColor: "red",
-        color: "#ffffff"
+        alignItems: "center"
+    },
+    image: {
+        width: 100,
+        height: 100,
+        borderRadius: 25
+    },
+
+    fundo: {
+        flex: 1,
+        resizeMode: 'cover',
+        justifyContent: "center",
+        alignItems: "center"
     }
+
 })
+
+export default Home;
